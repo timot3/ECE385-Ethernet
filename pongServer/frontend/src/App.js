33bda44e5a4667c09dac76243467416a0db0115c
Hassan F <1hassanfarooq@gmail.com>
2021-12-08 14:08:06 -0600

    Site looks better now

--- pongServer/frontend/src/App.js
+++ pongServer/frontend/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import Button from '@material-ui/core/Button';
+import arrow from "./arrow.svg"
 
 const sendUrl = "http://192.168.0.124:3080/sendPress";
+// const sendUrl = "http://alma.lol:120/sendPress";
 
-class Zoom extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       player: 1,
       direction: 0,
+      playerTxt: "Player 1"
+    }
+
+    this.uparrowStyle = {
+      width:251,
+      height:300,
+      background: `url(${arrow})`,
+      transform: "rotate(270deg)",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "auto",
+      backgroundPosition: "center",
+    };
+
+    this.downarrowStyle = {
+      width:251,
+      height:300,
+      background: `url(${arrow})`,
+      transform: "rotate(90deg)",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "auto",
+      backgroundPosition: "center",
+    };
+
+    this.noSelect = {
+      MozUserSelect: "none",
+      WebkitUserSelect: "none",
+      msUserSelect: "none",
     }
   }
 
@@ -45,21 +74,34 @@ class Zoom extends React.Component {
   }
 
   render() {
-    return <div className="zoomControl" >
-      <div className="zoom"></div>
+    return(
+      <div className="zoomControl" style={this.noSelect}>
+        <div className="zoom"></div><center>
 
-      <Button variant="contained" onClick={async () => {
-        this.setState({ player: 1 });
-      }}>set p1</Button>
+        <Button className="zoomIn" style={this.uparrowStyle} onMouseUp={() => this.onMouseUp()} onMouseDown={() => this.onMouseDown(1)}></Button>
+        <br></br>
+        <Button className="zoomIn"style={this.downarrowStyle} onMouseUp={() => this.onMouseUp()} onMouseDown={() => this.onMouseDown(2)}></Button>
 
-      <Button variant="contained" onClick={async () => {
-        this.setState({ player: 2 });
-      }}>set p2</Button>
+        <br></br><br></br>
+        
+        <Button variant="contained" onClick={async () => {
+          this.setState({ player: 1 });
+          this.setState({ playerTxt: "Player 1"});
+        }}>Set Player 1</Button>
 
-      <button className="zoomIn" onMouseUp={() => this.onMouseUp()} onMouseDown={() => this.onMouseDown(1)}>up</button>
-      <button className="zoomIn" onMouseUp={() => this.onMouseUp()} onMouseDown={() => this.onMouseDown(2)}>down</button>
-    </div>
+        <Button variant="contained" onClick={async () => {
+          this.setState({ player: 2 });
+          this.setState({ playerTxt: "Player 2"});
+        }}>Set Player 2</Button>
+        <br></br>
+        <p>Currently {this.state.playerTxt}</p>
+
+        <br></br><br></br><br></br><br></br><br></br><br></br>
+        <small>ECE 385 FA21 Final Project - Ethernet of DE10-Lite</small><br></br>
+        <small>Hassan Farooq, Tim Vitkin</small>
+      </center></div>
+    )
   }
 }
 
-export default Zoom;
+export default App;
